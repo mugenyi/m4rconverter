53de055fe667fb4949e186bb844bb1764e17d694
mugenyi <kraytosda1st@gmail.com>
2016-09-19 12:15:57 +0000

    add validation for the destination folder

--- src/Processor/FFmpegProcessor.php
+++ src/Processor/FFmpegProcessor.php
@@ -17,5 +17,35 @@ class FFmpegProcessor implements ProcessorInterface {
       return $this->FFMpeg->open($file);
     }
 
+    public function getFileNameWithOutExtention ()
+    {
+      list($name,$extention) = explode('.',basename($this->inputFile));
+      return $name;
+    }
+    private function validateDirectory($directory)
+    {
+
+      if(!is_dir($directory)){
+        throw new \Exception("destination is not a directory");
+
+      }
+
+      if(!is_writeable($directory)){
+        throw new \Exception("Could not write to the directory");
+
+      }
+      return true;
+    }
+
+    private function getOutPutFileName($destination){
+
+
+    }
+
+
+    public function save($format,$destination)
+    {
+        $this->FFMpeg->save($format,$des)
+    }
 
 }
