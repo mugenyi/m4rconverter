ce4baca35766e65022b1b7737adb9c53ecf43727
mugenyi <kraytosda1st@gmail.com>
2016-09-19 08:48:45 +0000

    add method to get only ffmpeg configurations

--- src/Converter.php
+++ src/Converter.php
@@ -1,9 +1,13 @@
 <?php
     namespace M4rconverter;
 
-
+     use FFMpeg\FFMpeg;
     class converter
     {
+      protected $file ;
+      protected $destination;
+      protected $configuration;
+
       /*
        * @param $inputFile String | Path to the file to be converted
        * @param $destination String| path to the destination  folder
@@ -12,6 +16,19 @@
 
       public function __construct($inputFile,$destination,$configuration=[])
       {
+          $this->file = $inputFile;
+          $this->destination = $destination;
+          $this->configuration  = $configuration;
+      }
 
+      protected getFFMpegConfiguration()
+      {
+        if(array_key_exists('bitrate',$this->configuration)){
+          unset($this->configuration['bitrate']);
+        }
+
+        if(array_key_exists('audioChannel')){
+          unset($this->configuration['audioChannel']);
+        }
       }
     }
