1eadb8f765bd32f0cedb243883bdbda37d802310
mugenyi <kraytosda1st@gmail.com>
2016-09-19 11:07:33 +0000

    add AAc audio format and setter for configurations

--- src/Format/M4r.php
+++ src/Format/M4r.php
@@ -2,9 +2,36 @@
    namespace M4rconverter\Format\Audio;
 
    use  FFMpeg\Format\Audio;
-   class M4r extends DefaultAudio{
+   class M4r
+   {
+      protected $configuration;
+      protected $format;
 
-      public function __construct($bitrate=null,$audioChannel=null){
-
+      public function __construct(array $configuration)
+      {
+        $this->configuration = $configuration;
+        $this->format =  new Aac();
       }
+
+      private function setConfiguration()
+      {
+        if(isset($this->configuration['bitrate'])){
+          $this->format->setAudioKiloBitrate($this->configuration['bitrate']);
+        }
+
+        if(isset($this->configuration['audioChannel'])){
+         $this->format->setAudioKiloBitrate($this->configuration['audioChannel']);
+        }
+
+        return $this;
+      }
+
+
+      public funtion getFormat()
+      {
+        return $this->format;
+      }
+
+
+
    }
