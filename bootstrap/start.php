bc5b36f8c631d6cea05d5570aa872b1fd0f93eae
mugenyi <kraytosda1st@gmail.com>
2016-09-21 17:36:37 +0000

    check for file existence before including it

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,2 +1,7 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+if(file_exists( __DIR__.'/../vendor/autoload.php')){
+  require_once __DIR__.'/../vendor/autoload.php';
+}else{
+  throw new \RuntimeException("please Install composer Packages");
+
+}
