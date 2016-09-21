acd8d1ccfe9b484a2486664a7ffa60b7ee3c8132
mugenyi <kraytosda1st@gmail.com>
2016-09-21 17:21:29 +0000

    refactor the Processor class and create Filemanager class
    For Better Testable code the FFmpegProcessor class has been
    divided. The code for managing files and directory has been
    placed in a new class which makes it easier to test the code

---
+++ src/FileManager.php
@@ -0,0 +1,53 @@
+<?php
+namespace M4rconverter;
+class FileManager
+{
+   protected $inputFile;
+   protected $destination;
+  public function __construct($file,$destination)
+  {
+    $this->inputFile = $file;
+    $this->destination = $destination;
+  }
+  public function getFileNameWithOutExtention ()
+  {
+    list($name,$extention) = explode('.',basename(  $this->inputFile ));
+    return $name;
+  }
+
+  public function validateDirectory()
+  {
+
+    if(!is_dir($this->destination)){
+      throw new \InvalidArgumentException("destination is not a directory");
+
+    }
+
+    if(!is_writable($this->destination)){
+      throw new \InvalidArgumentException("Could not write to the directory");
+
+    }
+    return true;
+  }
+
+  public function getDirectoryWithSeperator()
+  {
+    if(!preg_match("/.*\/$/",$this->destination)){
+      $this->destination = $this->destination.'/';
+    }
+
+    return $this->destination;
+  }
+
+
+
+
+  public function getOutPutFileName(){
+
+    $this->validateDirectory($this->destination);
+
+    return $this->getDirectoryWithSeperator()
+           .$this->getFileNameWithOutExtention().'.m4r';
+  }
+
+}
--- src/Processor/FFmpegProcessor.php
+++ src/Processor/FFmpegProcessor.php
@@ -4,7 +4,7 @@ use FFMpeg\FFMpeg;
 class FFmpegProcessor implements ProcessorInterface {
 
     protected $FFMpeg;
-    protected $inputFile;
+
 
     public function __construct($configuration)
     {
@@ -13,55 +13,14 @@ class FFmpegProcessor implements ProcessorInterface {
 
     public function getProcessedFile($file)
     {
-      $this->inputFile = $file;
       return $this->FFMpeg->open($file);
     }
 
-    public function getFileNameWithOutExtention ()
+
+
+    public function save($processedFile,$format,$outputFile)
     {
-      list($name,$extention) = explode('.',basename($this->inputFile));
-      return $name;
-    }
-
-    private function validateDirectory($directory)
-    {
-
-      if(!is_dir($directory)){
-        throw new \Exception("destination is not a directory");
-
-      }
-
-      if(!is_writable($directory)){
-        throw new \Exception("Could not write to the directory");
-
-      }
-      return true;
-    }
-
-    private function getDirectoryWithSeperator($directory)
-    {
-      if(!preg_match("/.*\/$/",$directory)){
-        $directory = $directory.'/';
-      }
-
-      return $directory;
-    }
-
-
-
-
-    private function getOutPutFileName($destination){
-
-      $this->validateDirectory($destination);
-
-      return $this->getDirectoryWithSeperator($destination)
-             .$this->getFileNameWithOutExtention().'.m4r';
-    }
-
-
-    public function save($processedFile,$format,$destination)
-    {
-        $processedFile->save($format->getFormat(),$this->getOutPutFileName($destination));
+        $processedFile->save($format->getFormat(),$outputFile);
     }
 
 }
