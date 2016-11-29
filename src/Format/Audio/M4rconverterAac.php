b7802321801b5f613d84bd50f5ba0a8b253da97c
mugenyi henry <kraytosda1st@gmail.com>
2016-11-29 17:11:05 +0300

    use better codec for encoding audio

--- src/Format/Audio/M4rconverterAac.php
+++ src/Format/Audio/M4rconverterAac.php
@@ -7,23 +7,25 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace M4rconverter\Format\Audio;
 
 use FFMpeg\Format\Audio\Aac;
 
 class M4rconverterAac extends Aac
 {
-  protected $params = ['-f', 'mp4'];
-  public function __construct(array $options =[])
-  {
-    $this->params =  array_merge($this->params,$options);
-  }
+    protected $params = ['-acodec', 'libfdk_aac', '-f', 'ipod'];
+    public function __construct(array $options = [])
+    {
+        $this->params = array_merge($this->params, $options);
+    }
   /**
-  * Add more options to the comandline ffmpeg command
-  * @return array
-  */
+   * Add more options to the comandline ffmpeg command.
+   *
+   * @return array
+   */
   public function getExtraParams()
   {
-    return $this->params;
+      return $this->params;
   }
 }
